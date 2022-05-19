943715dae2d117d8ce8a4efd27e6def59e253621
stoogebag <stoogebag@gmail.com>
2022-05-19 14:25:39 +1000

    units started

---
+++ stoogebag/Units.cs
@@ -0,0 +1,16 @@
+﻿namespace stoogebag
+{
+    public static class Units {
+
+        public static float KMHToMetresPerSecond(this float speed)
+        {
+            return speed / 3.6f;
+        }
+        public static float MetresPerSecondToKMH(this float speed)
+        {
+            return speed * 3.6f;
+        }
+
+
+    }
+}
