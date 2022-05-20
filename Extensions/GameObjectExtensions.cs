f1783979870bb25c1f1f147593c2fc7f35092577
stoogebag <stoogebag@gmail.com>
2022-05-20 15:59:40 +1000

    go extensions stub

---
+++ Extensions/GameObjectExtensions.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace stoogebag
+{
+    public static class GameObjectExtensions { 
+        //public static string TryGetGuid(this GameObject me)
+        //{
+        //    var gc = me.GetComponent<GuidComponent>();
+        //}
+    
+    
+    }
+
+}
