7fc0b2b375f4a934eca9ef3c8c3481dadc049bae
stoogebag <stoogebag@gmail.com>
2022-05-24 22:44:47 +1000

    simple components

---
+++ Common/StartVelocity.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class StartVelocity : MonoBehaviour
+{
+	private Rigidbody rb;
+	// Use this for initialization
+	Vector3 startDirection = Vector3.zero;
+	float startSpeed = 1;
+	
+	private void Awake()
+	{
+		rb = GetComponent<Rigidbody>();
+	}
+
+	void Start ()
+	{
+		var dir = startDirection;
+		if (dir == Vector3.zero) dir = transform.forward;
+		rb.velocity = dir * startSpeed;
+	}
+	
+	// Update is called once per frame
+}
