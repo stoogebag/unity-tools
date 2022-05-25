b5d18521f71cd6b4a0586863c02b6feeb2b0878a
stoogebag <stoogebag@gmail.com>
2022-05-25 10:44:20 +1000

    RandomExtensions

---
+++ Extensions/RandomExtensions.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace stoogebag
+{
+    public static class RandomExtensions
+    {
+        public static Random random = new Random();
+        public static T RandomSample<T>(this IProbabilityDistribution<T> dist)
+        {
+            var total = dist.GetTotalWeight();
+
+            var x = random.NextDouble();
+            var val = 0f;
+            foreach (var (item, weight) in dist.Weights)
+            {
+                val += weight/total;
+                if (val > x) return item;
+            }
+
+            throw new Exception("something is wrong lmao");
+        }
+    }
+
+    public interface IProbabilityDistribution<T>
+    {
+        public float GetTotalWeight();
+        public IEnumerable<(T, float)> Weights { get; }
+    }
+    public class SimpleProbabilityDist<T> :IProbabilityDistribution<T>
+    {
+        public List<T> Values;
+        
+        IEnumerable<(T, float)> IProbabilityDistribution<T>.Weights => Weights;
+        public List<(T, float)> Weights;
+
+        public float GetTotalWeight()
+        {
+            return Weights.Sum(t => t.Item2);
+        }
+
+    }
+
+}
