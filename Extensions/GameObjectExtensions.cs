92d45dca4f729abc928eee66022d7fbe557fe79b
stoogebag <stoogebag@gmail.com>
2022-06-06 20:29:31 +1000

    recursive child finding

--- Extensions/GameObjectExtensions.cs
+++ Extensions/GameObjectExtensions.cs
@@ -31,5 +31,22 @@ namespace stoogebag
             else go.layer = layer;
         }
     
+        public static Transform FirstOrDefault(this Transform transform, Func<Transform, bool> query)
+        {
+            if (query(transform)) {
+                return transform;
+            }
+
+            for (int i = 0; i < transform.childCount; i++)
+            {
+                var result = FirstOrDefault(transform.GetChild(i), query);
+                if (result != null)
+                {
+                    return result;
+                }
+            }
+
+            return null;
+        }
     }
 }
