764760c631d68e9e26a1dbd293c1bab189a7cd5e
stoogebag <stoogebag@gmail.com>
2022-06-14 12:27:54 +1000

    fade me

---
+++ Common/CameraFade.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CameraFade : MonoBehaviour {
+
+	public Image myImage;
+
+	public bool FadeOnStart = true;
+	
+	// Use this for initialization
+	void Start () {
+		myImage = GetComponent<Image> ();
+		if(FadeOnStart) FadeIn(Color.white, 1, null);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void FadeIn(Color startColour, float fadeTime, Action onFinish){
+		StartCoroutine(FadeInCoroutine(startColour, fadeTime, onFinish));
+	}
+		
+	public void FadeOut(Color startColour, float fadeTime, Action onFinish){
+		StartCoroutine(FadeOutCoroutine(startColour, fadeTime, onFinish));
+	}
+
+	//from colour to trans
+	IEnumerator FadeInCoroutine(Color startColour, float fadeTime, Action onFinish){
+		float t = 0;
+		myImage.enabled = true;
+
+		while (t < fadeTime) {
+			t += Time.deltaTime;
+			myImage.color = new Color (startColour.r,startColour.g,startColour.b, 1- t/fadeTime);
+			yield return null;
+		}
+		myImage.enabled = false;
+		onFinish?.Invoke();
+		yield break;
+	}
+
+
+	//from colour to trans
+	IEnumerator FadeOutCoroutine(Color startColour, float fadeTime, Action onFinish){
+		float t = 0;
+		myImage.enabled = true;
+
+		while (t < fadeTime) {
+			t += Time.deltaTime;
+			myImage.color = new Color (startColour.r,startColour.g,startColour.b, t/fadeTime);
+			yield return null;
+		}
+	//	myImage.enabled = false;
+		onFinish?.Invoke();
+		yield break;
+	}
+
+}
