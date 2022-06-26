6db27063c7b1210b1ea90c135bce0aafd5d74e95
stoogebag <stoogebag@gmail.com>
2022-06-26 12:11:12 +1000

    prefab stuff

---
+++ Editor/EditorMenus.cs
@@ -0,0 +1,24 @@
+using System;
+using UnityEditor;
+using UnityEngine;
+
+namespace stoogebag
+{
+    public class EditorMenus
+    {
+        [MenuItem("GameObject/Save As Prefab")]
+        public static void SaveAsPrefab()
+        {
+            var gameObject = Selection.activeGameObject;
+            SaveAsPrefab(gameObject);
+        }
+        public static void SaveAsPrefab(GameObject gameObject)
+        {
+            var folderPath = "Assets/Prefabs/Saved";
+            EditorTools.CreateFolder(folderPath);
+            var path = folderPath + "/" + gameObject.name + "-" + DateTime.Now.ToString("hh-mm-ss") + ".prefab";
+            PrefabUtility.SaveAsPrefabAsset(gameObject, path);
+        }
+
+    }
+}
