36328ddb0a1a4a3b6f12a175e4ae70f7b4e24309
stoogebag <stoogebag@gmail.com>
2023-10-10 15:11:28 +1100

    keycombo

---
+++ Extensions/InputExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+using UnityEngine;
+
+namespace stoogebag.Extensions
+{
+    public static class InputExtensions
+    {
+
+        public static bool GetKeyComboDown(params KeyCode[] keys)
+        {
+            return GetKeyCombo(keys) && keys.Any(k => UnityEngine.Input.GetKeyDown(k));
+        }
+        
+        public static bool GetKeyCombo(params KeyCode[] keys)
+        {
+            foreach (var key in keys)
+            {
+                if (!UnityEngine.Input.GetKey(key)) return false;
+            }
+
+            return true;
+        }
+
+
+    }
+}
