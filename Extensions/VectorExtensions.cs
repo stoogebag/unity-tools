55115a0ffb86d78393e8fb73bfe4d664d3a5fec4
stoogebag <stoogebag@gmail.com>
2024-02-16 14:03:57 +1100

    camera RelativeTo

--- Extensions/VectorExtensions.cs
+++ Extensions/VectorExtensions.cs
@@ -168,5 +168,20 @@ namespace stoogebag.Extensions
             Debug.DrawLine(point, proj, Color.red);
             return (t, proj);
         }
+        
+        
+        /// <summary>
+        /// Transform a given vector to be relative to target transform.
+        /// Eg: Use to perform movement relative to camera's view direction.
+        /// </summary>
+        public static Vector3 RelativeTo(this Vector3 vector3, Transform target, bool onlyLateral = true)
+        {
+            var forward = target.forward;
+
+            if (onlyLateral)
+                forward = Vector3.ProjectOnPlane(forward, Vector3.up);
+
+            return Quaternion.LookRotation(forward) * vector3;
+        }
     }
 }
