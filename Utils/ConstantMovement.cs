a91327d1b26e5871986b1b46720a2282e13a5fc4
stoogebag <stoogebag@gmail.com>
2025-11-22 00:33:02 +1100

    ispeedprovider

--- Utils/ConstantMovement.cs
+++ Utils/ConstantMovement.cs
@@ -2,10 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ConstantMovement : MonoBehaviour
+public class ConstantMovement : MonoBehaviour, ISpeedProvider
 {
 
     [SerializeField] private bool relative = false;
+    
     public Vector3 speed = new Vector3(10, 10, 10);
 
     // Update is called once per frame
@@ -16,4 +17,14 @@ public class ConstantMovement : MonoBehaviour
         else 
             transform.position = transform.position + speed * Time.fixedDeltaTime;
     }
+
+    public void SetSpeed(Vector3 newSpeed)
+    {
+        speed = newSpeed;
+    }
+}
+
+public interface ISpeedProvider
+{
+    public void SetSpeed(Vector3 newSpeed);
 }
