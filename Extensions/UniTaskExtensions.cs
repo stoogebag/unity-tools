96dd1a000c4c3baa560648e59115eabf441e388b
stoogebag <stoogebag@gmail.com>
2025-12-16 01:19:21 +1100

    awaitcondition

--- Extensions/UniTaskExtensions.cs
+++ Extensions/UniTaskExtensions.cs
@@ -1,4 +1,5 @@
 ﻿#if UNITASK
+using System;
 using System.Collections.Generic;
 using Cysharp.Threading.Tasks;
 
@@ -26,6 +27,14 @@ namespace stoogebag.Extensions
                 await uniTask;
             }
         } 
+        
+        public static async UniTask AwaitCondition(Func<bool> condition, int checkIntervalMs = 10)
+        {
+            while (!condition())
+            {
+                await UniTask.Delay(checkIntervalMs);
+            }
+        }
         
     }
 }
