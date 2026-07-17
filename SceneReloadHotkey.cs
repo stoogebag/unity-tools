51350b141dfff7209cb30f17039e02a9d64d1870
stoogebag <stoogebag@gmail.com>
2026-07-17 21:40:09 +1000

    scenereload. nb this is chromata specific please fix

--- SceneReloadHotkey.cs
+++ SceneReloadHotkey.cs
@@ -11,23 +11,48 @@ public class SceneReloadHotkey : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-         if (Input.GetKeyDown(hotkey) )
-         {
-             if (DestroyPersistentObjects)
-             {
-                 var temp = new GameObject();
-                 DontDestroyOnLoad(temp);
-                 var persistentScene = temp.scene;
-                 DestroyImmediate(temp);
-
-                 foreach (var root in persistentScene.GetRootGameObjects())
-                 {
-                     DestroyImmediate(root);
-                 }
-             }
-             
-             UnityEngine.SceneManagement.SceneManager.LoadScene(
-                 UnityEngine.SceneManagement.SceneManager.GetActiveScene().name);
+        if (Input.GetKeyDown(hotkey) )
+        {
+            
+            ReloadCurrentScene(DestroyPersistentObjects);
         }   
+        if (Input.GetKeyDown(KeyCode.B) )
+        {
+            if (DestroyPersistentObjects)
+            {
+                var temp = new GameObject();
+                DontDestroyOnLoad(temp);
+                var persistentScene = temp.scene;
+                DestroyImmediate(temp);
+
+                foreach (var root in persistentScene.GetRootGameObjects())
+                {
+                    DestroyImmediate(root);
+                }
+            }
+             
+            UnityEngine.SceneManagement.SceneManager.LoadScene("GeekMarketInfoScene");
+        }   
+    }
+
+    public static void ReloadCurrentScene(bool destroyPersistentObjects)
+    {
+        
+        if (destroyPersistentObjects)
+        {
+            var temp = new GameObject();
+            DontDestroyOnLoad(temp);
+            var persistentScene = temp.scene;
+            DestroyImmediate(temp);
+
+            foreach (var root in persistentScene.GetRootGameObjects())
+            {
+                DestroyImmediate(root);
+            }
+        }
+             
+        UnityEngine.SceneManagement.SceneManager.LoadScene(
+            UnityEngine.SceneManagement.SceneManager.GetActiveScene().name);
+        
     }
 }
