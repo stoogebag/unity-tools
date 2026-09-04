34ffadf101ff87bbdd47d0744ff69ef63bba8794
stoogebag <stoogebag@gmail.com>
2026-09-04 11:36:30 +1000

    managedupdate fixes

--- ManagedUpdate/IFixedUpdateManaged.cs
+++ ManagedUpdate/IFixedUpdateManaged.cs
@@ -3,5 +3,10 @@ namespace Stoogebag.ManagedUpdate
     public interface IFixedUpdateManaged
     {
         void ManagedFixedUpdate();
+
+        /// <summary>
+        /// Called once per component type to create the per-type manager.
+        /// </summary>
+        ManagerBase CreateManager();
     }
 }
--- ManagedUpdate/ILateUpdateManaged.cs
+++ ManagedUpdate/ILateUpdateManaged.cs
@@ -3,5 +3,10 @@ namespace Stoogebag.ManagedUpdate
     public interface ILateUpdateManaged
     {
         void ManagedLateUpdate();
+
+        /// <summary>
+        /// Called once per component type to create the per-type manager.
+        /// </summary>
+        ManagerBase CreateManager();
     }
 }
